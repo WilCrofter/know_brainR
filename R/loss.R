58fb76c429bdf94034c8002edb0a19b8e37d1eab
wilcrofter <bill@crofter.net>
2014-11-29 11:25:49 -0500

    references for approximating photon loss

---
+++ R/loss.R
@@ -0,0 +1,13 @@
+# Structural analysis of the thickness of human dura mater with scanning electron microscopy
+# http://www.ncbi.nlm.nih.gov/pubmed/8815466
+#
+# Calvarial thickness and its relation to cranial bone harvest.
+# http://www.ncbi.nlm.nih.gov/pubmed/16651971
+# 
+# Behaviour of near-infrared light in the adult human head:
+# implications for clinical near-infrared spectroscopy
+# http://www.ncbi.nlm.nih.gov/pubmed/10740545
+#
+# Cranial bone 6.3 mm
+# Dura .27 mm
+# Cerebral Cortex .29 mm
