48ec84197a879ee7a8f026723ca29830c675a7fc
wil <wil@crofter.net>
2015-08-15 17:09:48 -0400

    Utility to prepare BrainWeb phantom Area 17 for voxel-level simulation.

--- R/area17.R
+++ R/area17.R
@@ -1,28 +1,21 @@
+## Utility to prepare BrainWeb phantom Area 17 for voxel-level simulation.
 
-# Return a volume of the BrainWeb phantom which contains its primary visual cortex
-# (area 17 or V1.) Note, the returned volume is of type raw.
-getArea17 <- function(fname=NA){
-  if(is.na(fname) & file.exists("data/subject04_crisp_v.rawb")){
-    fname <- "data/subject04_crisp_v.rawb"
-  } else if(is.na(fname) & file.exists("../data/subject04_crisp_v.rawb")){
-    fname <- "../data/subject04_crisp_v.rawb"
-  } else {
-    stop("subject04_crisp_v.rawb was not found. Use getArea17(fname) where fname is the path to the file.")
+#' Return a volume of the BrainWeb phantom which contains its primary visual cortex
+#' (area 17) with foveal area properly stained.
+#' @param fname_BrainWeb_Phantom path to the BrainWeb file
+#' @param fovealTissueID an integer between 21 and 34 (inclusive) indicating level of dye concentration. 
+getBrainWebArea17 <- function(fname_BrainWeb_Phantom, fovealTissueID){
+  a17 <- readBin(fname_BrainWeb_Phantom, what="raw", 
+                  n=362*434*362, size=1, signed=FALSE, endian="big")
+  dim(a17)<-c(362,434,362)
+  a17 <- a17[(165-19):(165+19), 11:80, (125-19):(125+19)]
+  # The foveal area of the BrainWeb phantom's primary visual cortex appears
+  # to be ~1 cm (10 voxels) on a side centered around the volume's y axis at x=z=20,
+  # and consisting of gray matter voxels of minimum x coordinate in this volume.
+  for(ix in seq(20-10, 20+10)){
+    for(iz in seq(20-10, 20+10)){
+      a17[ix, min(which(as.integer(a17[ix,,iz])==3))] <- fovealTissueID
+    }
   }
-  temp <- readBin(fname, what="raw", n=362*434*362, size=1, signed=FALSE, endian="big")
-  dim(temp)<-c(362,434,362)
-  temp[(165-19):(165+19), 11:80, (125-19):(125+19)]
+  a17
 }
-
-# The foveal area of the BrainWeb phantom's primary visual cortex appears
-# to be ~1 cm (10 voxels) on a side centered around the volume's y axis at x=z=20,
-# and consisting of gray matter voxels of minimum x coordinate in this volume.
-# This function returns their indices.
-fovealRegion <- function(area17){
-  ans <- cbind(x_index=rep(seq(20-10, 20+10),20), y_index=NA, z_index=rep(seq(20-10, 20+10),each=20))
-  for(n in 1:nrow(ans)){
-    ans[n,2] <- min(which(as.integer(area17[ans[n,1],,ans[n,3]]) == 3))
-  }
-  ans
-}
-
