09e2bbb9b6624c2505c1b6012e752e68af59e1df
wil <wil@crofter.net>
2015-08-16 13:09:09 -0400

    utilities for area 17 BW phantom

--- R/area17.R
+++ R/area17.R
@@ -4,7 +4,7 @@
 #' (area 17) with foveal area properly stained.
 #' @param fname_BrainWeb_Phantom path to the BrainWeb file
 #' @param fovealTissueID an integer between 21 and 34 (inclusive) indicating level of dye concentration. 
-getBrainWebArea17 <- function(fname_BrainWeb_Phantom, fovealTissueID){
+brainWebArea17 <- function(fname_BrainWeb_Phantom, fovealTissueID){
   a17 <- readBin(fname_BrainWeb_Phantom, what="raw", 
                   n=362*434*362, size=1, signed=FALSE, endian="big")
   dim(a17)<-c(362,434,362)
@@ -12,10 +12,35 @@ getBrainWebArea17 <- function(fname_BrainWeb_Phantom, fovealTissueID){
   # The foveal area of the BrainWeb phantom's primary visual cortex appears
   # to be ~1 cm (10 voxels) on a side centered around the volume's y axis at x=z=20,
   # and consisting of gray matter voxels of minimum x coordinate in this volume.
-  for(ix in seq(20-10, 20+10)){
-    for(iz in seq(20-10, 20+10)){
-      a17[ix, min(which(as.integer(a17[ix,,iz])==3))] <- fovealTissueID
+  fovealTissueID <- as.raw(fovealTissueID)
+  for(ix in seq(20-5, 20+5)){
+    for(iz in seq(20-5, 20+5)){
+      a17[ix, min(which(as.integer(a17[ix,,iz])==3)), iz] <- fovealTissueID
     }
   }
   a17
 }
+
+#' Convert an array from type raw to type numeric
+#' @param raw_array an array of type raw
+#' @return an equivalent array of type numeric
+raw2numeric <- function(raw_array){
+  d <- dim(raw_array)
+  raw_array <- as.numeric(raw_array)
+  dim(raw_array) <- d
+  raw_array
+}
+
+#' Return the indices of the voxels on the scalp surface within
+#' an 11x11 square area surrounding voxel x=z=20 of the area17 volume.
+excitationArea17 <- function(e){
+  ans <- integer()
+  for(ix in seq(20-5, 20+5)){
+    for(iz in seq(20-5, 20+5)){
+      iy <- 1+max(which(e$state[1,ix,,iz]==0))
+      ans <- rbind(ans, c(ix, iy, iz))
+    }
+  }
+  ans
+}
+
